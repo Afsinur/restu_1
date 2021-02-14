79949ddaf460008b0ae137a9ed8aa00d8b7b83f8
mdafsinur <afsin.sayem1@gmail.com>
2021-02-14 16:04:05 +0600

    Alhamdulillah restu_1 v0.2 | added section2.

--- js/index.js
+++ js/index.js
@@ -29,6 +29,33 @@ const app = {
             },
           ],
         },
+        section2: {
+          banner: [
+            {
+              class1: "m-0 p-0 col-lg-6 col-md-12 sec2BigImg",
+              class2:
+                "p-0 m-0 col-lg-6 col-md-12 sec2Txt d-flex flex-column align-items-start justify-content-center",
+              src: "img/section2/top-left-ndls.png",
+              alt: "left_noodles",
+              h2: "Discount up to 50% All Menu",
+              p:
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem molestias, quo labore veritatis laborum sed vel accusantium aperiam delectus suscipit minus!",
+              a: "#",
+            },
+            {
+              class1:
+                "order-lg-2 order-1 m-0 p-0 col-lg-6 col-md-12 sec2BigImg",
+              class2:
+                "order-lg-1 order-2 p-0 m-0 col-lg-6 col-md-12 sec2Txt d-flex flex-column align-items-start justify-content-center",
+              src: "img/section2/bottom-left-ndls.png",
+              alt: "right_noodles",
+              h2: "January's Promo: Buy 1 Get 1 Free!",
+              p:
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem molestias, quo labore veritatis laborum sed vel accusantium aperiam delectus suscipit minus!",
+              a: "#",
+            },
+          ],
+        },
       },
     };
   },
